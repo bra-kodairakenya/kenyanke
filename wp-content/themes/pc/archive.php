438950ce5b0a46e65c1e229b8064a90712622c05
Braian <kenyanke.1@gmail.com>
2015-09-01 20:26:31 +0900

    PCのデザインをローカルで作成

---
+++ wp-content/themes/pc/archive.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>KURASHIRU</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/sanitize.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
+</head>
+
+<body id="body_layout">
+
+  <header>
+    <div id="header_in">
+      <div id="header_fixed">
+        <div id="header_logo">
+          <h1><a class="title-font" href="">コダイラ ケンヤ</a></h1>
+        </div>
+
+        <nav>
+          <ul id="nav_in">
+            <li class="nav-menu"><a class="big-font" href="" title="">制作実績</a></li>
+            <li class="nav-menu"><a class="big-font" href="" title="">活動の報告</a></li>
+            <li class="nav-menu"><a class="big-font" href="https://www.facebook.com/kenya0728" title="">Facebook</a></li>
+          </ul>
+        </nav>
+      </div><!-- header_fixed -->
+    </div><!-- header_in -->
+  </header>
+
+  <article>
+    <div id="wrapper">
+      <div id="archive">
+        <h1 class="title-font">記事一覧ページ</h1>
+        <article>
+          <div class="archive-in">
+            <a href="">
+              <h2 class="huge-font">記事のタイトルです</h2>
+            </a>
+          </div>
+        </article>
+        <article>
+          <div class="archive-in">
+            <a href="">
+              <h2 class="huge-font">記事のタイトルです</h2>
+            </a>
+          </div>
+        </article>
+        <article>
+          <div class="archive-in">
+            <a href="">
+              <h2 class="huge-font">記事のタイトルです</h2>
+            </a>
+          </div>
+        </article>
+        <article>
+          <div class="archive-in">
+            <a href="">
+              <h2 class="huge-font">記事のタイトルです</h2>
+            </a>
+          </div>
+        </article>
+      </div>
+    </div>
+  </article>
+
+  <footer>
+    <div id="footer">
+      <p id="copyright"><span class="small-font">Copyright(C) kenyanke, Inc. All Rights Reserved.</span></p>
+    </div>
+  </footer>
+
+</body>
+
+</html>
---
+++ wp-content/themes/pc/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>KURASHIRU</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/sanitize.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
+</head>
+
+<body id="body_layout">
+
+  <header>
+    <div id="header_in">
+      <div id="header_fixed">
+        <div id="header_logo">
+          <h1><a class="title-font" href="">コダイラ ケンヤ</a></h1>
+        </div>
+
+        <nav>
+          <ul id="nav_in">
+            <li class="nav-menu"><a class="big-font" href="" title="">制作実績</a></li>
+            <li class="nav-menu"><a class="big-font" href="" title="">活動の報告</a></li>
+            <li class="nav-menu"><a class="big-font" href="https://www.facebook.com/kenya0728" title="">Facebook</a></li>
+          </ul>
+        </nav>
+      </div><!-- header_fixed -->
+    </div><!-- header_in -->
+  </header>
+
+  <article>
+    <div id="wrapper">
+      <img id="background_img" src="images/106742y.jpg" alt="背景">
+      <img id="background_over_img" src="images/呑みに行こうぜ.jpg" alt="キャッチコピー">
+    </div>
+  </article>
+
+  <footer>
+    <div id="footer">
+      <p id="copyright"><span class="small-font">Copyright(C) kenyanke, Inc. All Rights Reserved.</span></p>
+    </div>
+  </footer>
+
+</body>
+
+</html>
